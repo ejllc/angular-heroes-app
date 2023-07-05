c02627868e7175cac7895c30fd203c710a05c7ad
ejllc <emiliano-javier.llorente-castelblanque@capgemini.com>
2023-07-05 09:42:51 +0200

    Estructura

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { Route, RouterModule, Routes } from "@angular/router";
+
+
+const routes: Routes = [
+
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class AuthRountingModule {
+
+}
